868ce882b81515fed88b38188cca074ca294b0b4
Relyful <13381270+Relyful@users.noreply.github.com>
2025-08-12 21:09:41 +0200

    Create controller and routes, add error middleware and catchall

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 const express = require('express');
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const path = require("path");
 
+
+const indexRouter = require('./routers/indexRouter');
+
+//dotenv setup
 require('dotenv').config();
 
 const PORT = process.env.port || 8080;
 
 const app = express();
+
+//Set-up url request body parsing
+app.use(express.urlencoded({ extended: false }));
+//Set-up Public files
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.use('/', indexRouter);
+
+//Catch all route
+app.get("/*splat", async (req, res) => {
+  res.send("You cannot be here :( .");
+});
+
+//Error middleware
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).render('errPage',{
+    errMessage: err.message
+  });
+});
+
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
