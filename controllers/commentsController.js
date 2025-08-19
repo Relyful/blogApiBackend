1daf20b939aef94241fff751209e0bfa74819f2b
Relyful <13381270+Relyful@users.noreply.github.com>
2025-08-19 21:54:57 +0200

    Add route to update own comment

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -13,3 +13,32 @@ exports.createComment = async (req, res) => {
   })
   return res.sendStatus(201);
 }
+
+exports.updateOwnComment = async (req, res) => {
+  const data = req.body;
+  const user = req.user;
+  const commentId = req.params.commentId;
+
+  const ownershipCheck = await prisma.comment.findFirst({
+    where: {
+      id: commentId,
+      authorId: user.id
+    }
+  })
+  if(!ownershipCheck) {
+    return res.sendStatus(404);
+  }
+  await prisma.comment.update({
+    where: {
+      id: commentId
+    },
+    data: {
+      message: data.message
+    }
+  })
+  return res.sendStatus(200);
+}
+
+// exports.removeOwnComment = async (req, res) => {
+
+// }
--- routers/commentsRouter.js
+++ routers/commentsRouter.js
@@ -4,5 +4,6 @@ const commentsController = require('../controllers/commentsController');
 const commentsRouter = Router();
 
 commentsRouter.post('/', commentsController.createComment);
+commentsRouter.put('/:commentId', commentsController.updateOwnComment);
 
 module.exports = commentsRouter;
