86f1534c6d3b1d1bb2e0dde462a565f846438805
Relyful <13381270+Relyful@users.noreply.github.com>
2025-09-25 21:48:52 +0200

    if user exists on register send specific response

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,36 +1,47 @@
-const { PrismaClient } = require('../generated/prisma');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const { PrismaClient } = require("../generated/prisma");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const prisma = new PrismaClient();
 
 exports.getIndex = (req, res) => {
-  res.send('Hello World :)');
-}
+  res.send("Hello World :)");
+};
 
 exports.postLogin = async (req, res) => {
   const userData = req.user;
-  const token = jwt.sign({id: userData.id, username: userData.username}, process.env.SECRET, { expiresIn: "1h"});
-  res.json({token, userData});
-}
+  const token = jwt.sign(
+    { id: userData.id, username: userData.username },
+    process.env.SECRET,
+    { expiresIn: "1h" },
+  );
+  res.json({ token, userData });
+};
 
 exports.postRegister = async (req, res) => {
   const data = req.body;
   console.log(req.body);
-  const hashedPassword = await bcrypt.hash(data.password, 10);
-  const user = await prisma.user.create({
-    data: {
-      username: data.username,
-      password: hashedPassword,
-      role: data.role
+  try {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    const user = await prisma.user.create({
+      data: {
+        username: data.username,
+        password: hashedPassword,
+        role: data.role,
+      },
+    });
+    console.log(user);
+    res.sendStatus(201);
+  } catch (error) {
+    if (error.code === 'P2002') {
+      return res.json({"error": 'Username already exists'});
     }
-  })
-  console.log(user);
-  res.sendStaus(201);
-}
+    throw error;
+  }
+};
 
 exports.authTest = async (req, res) => {
   res.json({
-    user: req.user
-  })
-}
+    user: req.user,
+  });
+};
