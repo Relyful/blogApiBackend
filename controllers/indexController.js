5da4740f7d29cf0f38e87e2242a458bb9d9d2621
Relyful <13381270+Relyful@users.noreply.github.com>
2025-11-05 21:39:37 +0100

    Trim register data

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,8 +1,17 @@
 const { PrismaClient } = require("../generated/prisma");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { body, validationResult } = require("express-validator");
 
 const prisma = new PrismaClient();
+
+const validateRegister = [
+  body("username")
+    .trim()
+    .isLength({ min: 5, max: 10 })
+    .withMessage("Username must be between 5 and 10 characters"),
+  body("password").trim(),
+];
 
 exports.getIndex = (req, res) => {
   res.send("Hello World :)");
@@ -18,30 +27,37 @@ exports.postLogin = async (req, res) => {
   res.json({ token, userData });
 };
 
-exports.postRegister = async (req, res) => {
-  const data = req.body;
-  console.log(req.body);
-  if (data.username.length < 4 || data.username.length > 10) {
-    return res.json({"error": "Username too short or too long"})
-  }
-  try {
-    const hashedPassword = await bcrypt.hash(data.password, 10);
-    const user = await prisma.user.create({
-      data: {
-        username: data.username,
-        password: hashedPassword,
-        role: data.role,
-      },
-    });
-    console.log(user);
-    res.status(201).json(user);
-  } catch (error) {
-    if (error.code === 'P2002') {
-      return res.json({"error": 'Username already exists'});
+exports.postRegister = [
+  validateRegister,
+  async (req, res) => {
+    const data = req.body;
+    console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({ error: "Username must be between 5 and 10 characters"});
     }
-    throw error;
-  }
-};
+    if (data.username.length < 4 || data.username.length > 10) {
+      return res.json({ error: "Username too short or too long" });
+    }
+    try {
+      const hashedPassword = await bcrypt.hash(data.password, 10);
+      const user = await prisma.user.create({
+        data: {
+          username: data.username,
+          password: hashedPassword,
+          role: data.role,
+        },
+      });
+      console.log(user);
+      res.status(201).json(user);
+    } catch (error) {
+      if (error.code === "P2002") {
+        return res.json({ error: "Username already exists" });
+      }
+      throw error;
+    }
+  },
+];
 
 exports.authTest = async (req, res) => {
   res.json({
